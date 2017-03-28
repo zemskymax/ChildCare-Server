9fb4035c14f3165d8fe2c202f4962757544d2dea
Zemsky <2trex3@net.plm.eds.com>
2017-03-28 13:57:29 +0200

    fix json

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -9,14 +9,14 @@ exports.get_devices_last_location = function(req,res,next){
 	console.log("get_devices_last_location");
 	
 	//TODO. Do something in the db
-    r.msg.push('we are ok');
-	r.info = {"info" : [
+    //r.msg.push('we are ok');
+	return res.json({"info" : [
 					{"id" : "mac1", "name" : "Raz", "location" : "reporter1", "time" : "1"},
 					{"id" : "mac2", "name" : "Max", "location" : "reporter2", "time" : "2"},
 					{"id" : "mac4", "name" : "Rony", "location" : "reporter3", "time" : "2"},
 					{"id" : "mac3", "name" : "Meir", "location" : "reporter4", "time" : "2"}
-					]};
-	r.status = 1;
-	return res.json(r);
+					]});
+	//r.status = 1;
+	//return res.json(r);
 }
 
