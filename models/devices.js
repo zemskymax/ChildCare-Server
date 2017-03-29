8d5f889b442312a25259bfe615b9d0c676d95cdd
Zemsky <2trex3@net.plm.eds.com>
2017-03-29 14:58:17 +0200

    fix a bug

--- models/devices.js
+++ models/devices.js
@@ -68,7 +68,7 @@ deviceSchema.statics.set_devices_last_location=function(reporterId,deviceReports
 					};
 					console.log("--deviceSchema - device : ", device);
 					
-					this.model('devices').findOneAndUpdate(query,{$set:device},options)
+					Device.findOneAndUpdate(query,{$set:device},options)
 						.exec(function(err,result) {
 							if (err) {
 							console.log("--deviceSchema - device lication was not updated. id: ", report.Address);
